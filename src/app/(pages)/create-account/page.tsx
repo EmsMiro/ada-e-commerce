6060df1b5d9447cc2a7f6c47b6e83299678b4339
EmsMiro <ednirlicosta@gmail.com>
2024-03-18 18:19:17 -0300

    enviando form de criação de conta finalizada

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -8,26 +8,50 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import CreateAccountForm from './CreateAccountForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function CreateAccount() {
   await getMeUser({
     validUserRedirect: `/account?warning=${encodeURIComponent(
-      'Cannot create a new account while logged in, please log out and try again.',
+      'Não é possível criar uma nova conta enquanto estiver conectado, faça logout e tente novamente.',
     )}`,
   })
 
   return (
-    <Gutter className={classes.createAccount}>
-      <h1>Create Account</h1>
-      <RenderParams />
-      <CreateAccountForm />
-    </Gutter>
+    <section className={classes.createAccount}>
+    <div className={classes.heroImg}>
+      <Link href="/">
+        <Image
+        src="/logo-black.svg"
+        alt='logo'
+        width={250}
+        height={23}
+        className={classes.logo} />
+      </Link>
+    </div>
+    <div className={classes.formWrapper}>
+      <div className={classes.formContainer}>
+        <RenderParams className={classes.params}/>
+        <div className={classes.formTitle}>
+          <h3>Crie sua conta</h3>
+          <Image src="/assets/icons/hand.png"
+          alt='hand'
+          width={30}
+          height={30}
+          />
+        </div>
+        <p>Crie sua conta aqui</p>
+        <CreateAccountForm/>
+      </div>
+      </div>
+  </section>
   )
 }
 
 export const metadata: Metadata = {
   title: 'Account',
-  description: 'Create an account or log in to your existing account.',
+  description: 'Crie uma conta ou faça login em sua conta existente.',
   openGraph: mergeOpenGraph({
     title: 'Account',
     url: '/account',
